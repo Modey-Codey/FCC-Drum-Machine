d015bd48ddee7d22571341465363c8316c1657ca
Modey Codey <phawngam@hotmail.com>
2024-02-19 16:59:35 +0700

    fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { AudioClip } from './types';
 import Drum from './Drum'
 
 const audioClips: AudioClip[] = [
